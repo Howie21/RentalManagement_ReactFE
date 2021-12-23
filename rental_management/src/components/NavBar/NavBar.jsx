61e0dc938dc105de5966c0c6f4b7d01e90520d3f
Howie21 <bryce9643@gmail.com>
2021-12-23 11:30:42 -0500

    Creation of NavBar class and scafolding

---
+++ rental_management/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown, Modal, Container} from 'react-bootstrap';
+
+//Component Imports
+
+
+function NavBar({user, logout}) {
+    return ( 
+        <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
+            <Container>
+                
+            </Container>
+        </Navbar>
+     );
+}
+
+export default NavBar;{user, logout}
