3b4314cd01cd9a07d65a1f4f58fbf9badb143538
Howie21 <bryce9643@gmail.com>
2021-12-23 12:39:02 -0500

    Added Conditional Rendering to Navbar // Added Navbar construction to App

--- rental_management/src/App.jsx
+++ rental_management/src/App.jsx
@@ -4,19 +4,51 @@ import {Route, Routes} from 'react-router-dom';
 import axios from 'axios';
 
 //Imports from component files
-
+import NavBar from "./components/NavBar/NavBar"
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      
+      user: '',
+      isLandLord: false,
+      property: '',
+
      }
   }
+
+  componentDidMount() {
+    const jwt = localStorage.getItem('token');
+    try {
+      this.getUser(jwt);
+    } 
+    catch {
+      console.log('Something went wrong // line 26 of App');
+    }
+  }
+
+  async getUser(token) {
+    let user = await axios({
+      method: 'GET',
+      url: 'https://localhost:44394/api/examples/user',
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    this.setState({
+      user: user.data,
+    });
+  }
+
+  logout = () => {
+    localStorage.removeItem('token');
+    this.setState({
+      user: '',
+    });
+  };
+
   render() { 
     return ( 
       <div className="App">
-        
+        <NavBar bg='light' expand='lg' user={this.state.user} landLordStatus={this.state.isLandLord} logout={this.logout}/>
       </div>
      );
   }
--- rental_management/src/components/NavBar/NavBar.jsx
+++ rental_management/src/components/NavBar/NavBar.jsx
@@ -4,13 +4,77 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown, Modal, Container} from 'react-bootstrap';
 
 //Component Imports
+import Login from "../Login/Login";
 
-
-function NavBar({user, logout}) {
+function NavBar({user, landLordStatus, logout}) {
+    const [showModel, setShowModel] = useState(false);
     return ( 
         <Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
             <Container>
-                
+                <Navbar.Brand as={Link} to="/">
+                    Howard Rental's
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id='responsive-navbar-nav' >
+                    <Nav>
+                        <Nav.link as={Link} to="/">
+                            Home
+                        </Nav.link>
+                        \
+                        {user && landLordStatus == false && (
+                            <>
+                                <Nav.Link as={Link} to="/TPropertyManagement">
+                                    Property Management
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/TPayment">
+                                    Payment
+                                </Nav.Link>
+                            </>
+                        )}
+                        {user && landLordStatus == true && (
+                            <>
+                                <Nav.Link as={Link} to="/LLPropertyManagement">
+                                    Property Management
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/TenantManagement">
+                                    Tenant Management
+                                </Nav.Link>
+                                <Nav.Link as={Link} to="/LLPaymentManagement">
+                                    Payment Management
+                                </Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                    <Nav>
+                        {user && (
+                            <>
+                                <NavDropdown title="Account" id="navbarScrollingDropdown">
+                                    <NavDropdown.Item as={Link} to="/Account">
+                                        Manage Account
+                                    </NavDropdown.Item>
+                                    <NavDropdown.Divider />
+                                    <NavDropdown.Item as={Link} to="/" onClick={() => logout()}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            </>
+                        )}
+                        {!user && (
+                            <>
+                                <Nav.Link as={Link} onClick={() => setShowModel(true)}>Login</Nav.Link>
+                                
+                            </>
+                        )}
+                    </Nav>
+                    <Modal show={showModel} onHide={() => setShowModel(false)} >
+                        <Modal.Header closeButton>
+                            <Modal.Title>Please Login</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Login />
+                        </Modal.Body>
+                    </Modal>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
      );
