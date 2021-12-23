aa0fccb3b498c676406581ea5ec1d0836beb75b8
Howie21 <bryce9643@gmail.com>
2021-12-23 16:49:07 -0500

    Creation of LandingPage.jsx and css File

---
+++ rental_management/src/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+function LandingPage() {
+    return ( 
+        <div>
+            <div className="row">
+                <h1>Welcome to Howard Rental's!</h1>
+            </div>
+            <div className="row">
+                <div className="col-lg-6">
+                    <h3>Current Tenant?</h3>
+                    <p className="text-center">
+                        If you are a Current Tenant and wish to login, press the login button in the top right hand corner of the page! 
+                    </p>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default LandingPage;
