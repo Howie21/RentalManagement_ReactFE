90e47471d34268dc912a43467d26ce6cf8007515
Howie21 <bryce9643@gmail.com>
2021-12-23 11:29:24 -0500

    Changed App file type to Jsx // App became a Stateful Component // Removed App.css, App will not be directly styled.

---
+++ rental_management/src/App.jsx
@@ -0,0 +1,25 @@
+//Imports from dependencies
+import React, { Component } from 'react';
+import {Route, Routes} from 'react-router-dom';
+import axios from 'axios';
+
+//Imports from component files
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      
+     }
+  }
+  render() { 
+    return ( 
+      <div className="App">
+        
+      </div>
+     );
+  }
+}
+ 
+export default App;
