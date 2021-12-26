f76cc24eae63fa7f67b4bf8ea9d9c2bc943fb69f
Howie21 <bryce9643@gmail.com>
2021-12-26 13:48:57 -0500

    Changed Imports on Index

--- rental_management/src/index.js
+++ rental_management/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 ReactDOM.render(
   <Router>
