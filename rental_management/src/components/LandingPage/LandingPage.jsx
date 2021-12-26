8dbea34248c1e23b82c45b0f834d3cc5f5288817
Howie21 <bryce9643@gmail.com>
2021-12-26 13:48:12 -0500

    Added Placeholder text to the Landing Page

--- rental_management/src/components/LandingPage/LandingPage.jsx
+++ rental_management/src/components/LandingPage/LandingPage.jsx
@@ -8,11 +8,17 @@ function LandingPage() {
                 <h1>Welcome to Howard Rental's!</h1>
             </div>
             <div className="row">
-                <div className="col-lg-6">
-                    <h3>Current Tenant?</h3>
+                <div className="col-lg-6 col-md-3 col-sm-2 row testDiv1">
+                    <h3 className=" text-center">Current Tenant?</h3>
                     <p className="text-center">
                         If you are a Current Tenant and wish to login, press the login button in the top right hand corner of the page! 
                     </p>
+                </div>
+                <div className="col-lg-6 col-md-3 col-sm-2 row testDiv2" >
+                    <p>Testing 2</p>
+                </div>
+                <div className="col-lg-6 col-md-3 col-sm-2 row testDiv3">
+                    <p>Testing 3</p>
                 </div>
             </div>
         </div>
