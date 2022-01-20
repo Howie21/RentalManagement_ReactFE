85cd205ffaab330b4a19a46f52309a64b62df03b
Howie21 <bryce9643@gmail.com>
2022-01-19 20:15:44 -0500

    Containers to Management on Tenant userface

--- src/components/T_PropertyManagement/T_PropertyManagement.jsx
+++ src/components/T_PropertyManagement/T_PropertyManagement.jsx
@@ -1,5 +1,6 @@
 //Dependency Imports
 import React from 'react';
+import { Container } from 'react-bootstrap';
 
 
 
@@ -11,27 +12,31 @@ function TPropertyManagement(props) {
                 <h3>Property Management Hub</h3>
                 <p>Here you can make your payment</p>
             </div>
-            <div>
-                <h4>Property:</h4>
-                <br/>
-                <ul>
-                    <li>Property Id: {props.property.id}</li>
-                    <li>Square Feet: {props.property.squareFeet}</li>
-                    <li>Bathrooms: {props.property.bathrooms}</li>
-                    <li>Bedrooms: {props.property.bedrooms}</li>
-                    <li>Address: {props.property.address.buildingNumber} {props.property.address.street} {props.property.address.city} {props.property.address.state}, {props.property.address.zipCode}</li>
-                </ul>
-            </div>
-            <div>
-                <h5>Lease Information</h5>
-                <ul>
-                    <li>Lease ID: {props.tenantInfo.lease.id}</li>
-                    <li>Lease Number: {props.tenantInfo.lease.leaseNumber}</li>
-                    <li>Start Date: {props.tenantInfo.lease.startDate}</li>
-                    <li>End Date: {props.tenantInfo.lease.endDate}</li>
-                    <li>Safety Deposit: {props.tenantInfo.lease.safetyDeposit}</li>
-                </ul>
-            </div>
+            <Container>
+                <div>
+                    <h4>Property:</h4>
+                    <br/>
+                    <ul>
+                        <li>Property Id: {props.property.id}</li>
+                        <li>Square Feet: {props.property.squareFeet}</li>
+                        <li>Bathrooms: {props.property.bathrooms}</li>
+                        <li>Bedrooms: {props.property.bedrooms}</li>
+                        <li>Address: {props.property.address.buildingNumber} {props.property.address.street} {props.property.address.city} {props.property.address.state}, {props.property.address.zipCode}</li>
+                    </ul>
+                </div>
+            </Container>
+            <Container>
+                <div>
+                    <h5>Lease Information</h5>
+                    <ul>
+                        <li>Lease ID: {props.tenantInfo.lease.id}</li>
+                        <li>Lease Number: {props.tenantInfo.lease.leaseNumber}</li>
+                        <li>Start Date: {props.tenantInfo.lease.startDate}</li>
+                        <li>End Date: {props.tenantInfo.lease.endDate}</li>
+                        <li>Safety Deposit: {props.tenantInfo.lease.safetyDeposit}</li>
+                    </ul>
+                </div>
+            </Container>
             <div>
                 <h5>Wanna make a payment? Click the button below!!</h5>
                 <button className='btn btn-secondary' onClick={() => window.location="/MakePayment"} >Make Payment</button>
