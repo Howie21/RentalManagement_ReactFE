69122009e86eb46323dc86a6d5a419c6f0e0349a
Howie21 <bryce9643@gmail.com>
2022-01-20 18:10:20 -0500

    Added extra styling and bounderies to PropertyManagement

--- src/components/T_PropertyManagement/T_PropertyManagement.jsx
+++ src/components/T_PropertyManagement/T_PropertyManagement.jsx
@@ -1,5 +1,7 @@
 //Dependency Imports
 import React from 'react';
+import {Container} from "react-bootstrap";
+
 
 
 
@@ -7,34 +9,42 @@ function TPropertyManagement(props) {
     
     return ( 
         <div>
+            <Container>
             <div className="text-center" >
                 <h3>Property Management Hub</h3>
                 <p>Here you can make your payment</p>
             </div>
-            <div>
-                <h4>Property:</h4>
-                <br/>
-                <ul>
-                    <li>Property Id: {props.property.id}</li>
-                    <li>Square Feet: {props.property.squareFeet}</li>
-                    <li>Bathrooms: {props.property.bathrooms}</li>
-                    <li>Bedrooms: {props.property.bedrooms}</li>
-                    <li>Address: {props.property.address.buildingNumber} {props.property.address.street} {props.property.address.city} {props.property.address.state}, {props.property.address.zipCode}</li>
-                </ul>
-            </div>
-            <div>
-                <h5>Lease Information</h5>
-                <ul>
-                    <li>Lease ID: {props.tenantInfo.lease.id}</li>
-                    <li>Lease Number: {props.tenantInfo.lease.leaseNumber}</li>
-                    <li>Start Date: {props.tenantInfo.lease.startDate}</li>
-                    <li>End Date: {props.tenantInfo.lease.endDate}</li>
-                    <li>Safety Deposit: {props.tenantInfo.lease.safetyDeposit}</li>
-                </ul>
+            </Container>
+            <div className="row">
+                <Container className="infoContainer">
+                <div className="col-6">
+                    <h5 className="text-center">Property:</h5>
+                    <br/>
+                    <ul>
+                        <li>Property Id: {props.property.id}</li>
+                        <li>Square Feet: {props.property.squareFeet}</li>
+                        <li>Bathrooms: {props.property.bathrooms}</li>
+                        <li>Bedrooms: {props.property.bedrooms}</li>
+                        <li>Address: {props.property.address.buildingNumber} {props.property.address.street} {props.property.address.city} {props.property.address.state}, {props.property.address.zipCode}</li>
+                    </ul>
+                </div>
+                </Container>
+                <Container className="infoContainer">
+                <div className="col-6">
+                    <h5 className="text-center">Lease Information</h5>
+                    <ul>
+                        <li>Lease ID: {props.tenantInfo.lease.id}</li>
+                        <li>Lease Number: {props.tenantInfo.lease.leaseNumber}</li>
+                        <li>Start Date: {props.tenantInfo.lease.startDate}</li>
+                        <li>End Date: {props.tenantInfo.lease.endDate}</li>
+                        <li>Safety Deposit: {props.tenantInfo.lease.safetyDeposit}</li>
+                    </ul>
+                </div>
+                </Container>
             </div>
             <div>
                 <h5>Wanna make a payment? Click the button below!!</h5>
-                <button className='btn btn-secondary' onClick={() => window.location="/MakePayment"} >Make Payment</button>
+                <button className='btn btn-success' onClick={() => window.location="/MakePayment"} >Make Payment</button>
             </div>
         </div>
      );
