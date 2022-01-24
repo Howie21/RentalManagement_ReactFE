4e1e580f734fa688d8842d44a1c4b1a9ed0a7ba0
Howie21 <bryce9643@gmail.com>
2022-01-24 17:40:42 -0500

    Added functionality to Payment history to properly show payments for users of tenant status

--- src/components/T_PaymentHistory/T_PaymentHistory.jsx
+++ src/components/T_PaymentHistory/T_PaymentHistory.jsx
@@ -1,6 +1,6 @@
 //Dependencies Imports
 import React, { Component } from 'react';
-
+import {Table, Container} from "react-bootstrap";
 
 
 
@@ -23,24 +23,30 @@ class T_PaymentHistory extends Component {
                     <h3>All Previous payments for the Year!</h3>
                 </div>
                 <br/>
-                <table id='paymentHistory' className='paymentTable'> 
-                    <tr className='cartTableHeader'>
-                        <th>Tenant</th>
-                        <th>Amount</th>
-                        <th>Date</th>
-                        <th>Property ID</th>
-                    </tr>
-                    {paymentHistory.map(pm => {
-                        return (
-                        <tr className='cartItem'>
-                            <td>{pm.user.FirstName} {pm.user.lastName}</td>
-                            <td>{pm.paymentAmount}</td>
-                            <td>{pm.dateAndTime}</td>
-                            <td>{pm.propertyId}</td>
-                        </tr>
-                        );
-                    })};
-                </table>
+                <Container>
+                    <Table>
+                        <thead>
+                            <tr className="text-center">
+                                <th>Tenant</th>
+                                <th>Amount</th>
+                                <th>Date</th>
+                                <th>Property Id</th> 
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {paymentHistory.map(pm => {
+                                return(
+                                    <tr className="text-center">
+                                        <td>{pm.user.firstName} {pm.user.lastName}</td>
+                                        <td>{pm.paymentAmount}</td>
+                                        <td>{pm.dateAndTime}</td>
+                                        <td>{pm.propertyId}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </Table>
+                </Container>
             </div>
          );
     };
